aac43fb988af84da3fec53e67e949db31e9acd7e
ldq3 <dequan_li.cn@outlook.com>
2024-08-04 16:22:05 +0800

    fix: modify var name

--- r4l_uart.rs
+++ r4l_uart.rs
@@ -22,7 +22,7 @@ pub(crate) static mut PORTS: [Option<uart_port::UartPort>; UART_NR] = [None; UAR
 
 module_amba_driver! {
     type: Pl011Driver,
-    name: "r4l_uart",
+    name: "pl011_driver",
     author: "ldq3",
     description: "pl011 driver based on Rust for Linux",
     license: "GPL",
@@ -54,27 +54,26 @@ impl amba::Driver for Pl011Driver {
     // type Data = Arc<DeviceData>;
 
     kernel::driver_amba_id_table!(MY_AMDA_ID_TABLE);
-    fn probe(adev: &mut amba::Device, _data: Option<&Self::IdInfo>) -> Result {
-        dev_info!(adev,"{} PL061 GPIO chip (probe)\n",adev.name());
-        dbg!("********** PL061 GPIO chip (probe) *********\n");
+    fn probe(adev: &mut amba::Device, _id_info: Option<&Self::IdInfo>) -> Result {
+        dev_info!(adev, "{} PL011 GPIO chip (probe)\n", adev.name());
 
         let dev = device::Device::from_dev(adev);
+        // TODO: vendor, id_info
 
         let portnr = pl011_find_free_port()?;
-        // dev_info!(adev,"portnr is {}\n",portnr);
-        let clk = dev.clk_get().unwrap();  // 获得clk
-        let fifosize = if adev.revision_get().unwrap() < 3 {16} else {32};
-        let iotype = UPIO_MEM as u8;
-        let reg_base = adev.take_resource().ok_or(ENXIO)?;
+        dev_info!(adev,"portnr is {}\n",portnr);
+
+        // uap
+        let clk = dev.clk_get().unwrap();
+        let reg_base   = adev.take_resource().ok_or(ENXIO)?;
         let reg_mem : io_mem::IoMem<UART_SIZE>= unsafe { io_mem::IoMem::try_new(&reg_base)?};
         let mapbase = reg_base.get_offset();
         let membase = reg_mem.get();
+        let fifosize = if adev.revision_get().unwrap() < 3 {16} else {32};
+        let iotype = UPIO_MEM as u8;
         let irq = adev.irq(0).ok_or(ENXIO)?;
+        // TODO: ops
 
-        dev_info!(adev,"fifosize is {}\n",fifosize);
-        dev_info!(adev,"mapbase is 0x{:x}\n", mapbase);
-        dev_info!(adev,"membase is 0x{:p}\n",membase);
-        dev_info!(adev,"irq is {}\n",irq);
         let has_sysrq = 1;
         let flags = UPF_BOOT_AUTOCONF;
         let port =  uart_port::UartPort::new().setup(
@@ -86,9 +85,9 @@ impl amba::Driver for Pl011Driver {
                 has_sysrq,
                 fifosize,
              portnr as _,
-            );     
+            );
 
-        dbg!("********* PL061 GPIO chip registered *********\n");
+        dbg!("********* PL011 GPIO chip registered *********\n");
         Ok(())
     }
 }
@@ -96,6 +95,6 @@ impl amba::Driver for Pl011Driver {
 impl Drop for Pl011Driver {
     fn drop(&mut self) {
         pr_info!("My numbers are {:?}\n", self.numbers);
-        pr_info!("UART driver module (exit)\n");
+        pr_info!("pl011 driver module (exit)\n");
     }
 }
